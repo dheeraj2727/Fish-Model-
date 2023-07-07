9ea139ca49b1e4ec9ed5b00558a01e9b2f7b1055
dheeraj2727 <127006472+dheeraj2727@users.noreply.github.com>
2023-07-07 11:59:00 +0530

    Add files via upload

--- config.py
+++ config.py
@@ -1,9 +1,9 @@
 import os
 
-PORT_NUMBER = 5000
+PORT_NUMBER = 8080
 
 MONGODB_PORT_NUMBER = 27017
 MONGODB_DATABASE_NAME = 'project_db'
-MODEL_FILE_PATH = r"D:\Datascience\Classpractice\Regression\Fish data\fish_model.pkl"
+MODEL_FILE_PATH = "fish_model.pkl"
 
-JSON_FILE_PATH = r"D:\Datascience\Classpractice\Regression\Fish data\proj_data.json"
+JSON_FILE_PATH = "proj_data.json"
